7a30c87fdc710e094ba5afc38e259b8ec7f36435
Jair Sánchez <jssanchez9@espe.edu.ec>
2024-06-17 22:30:45 -0500

    Componenete tabla final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Tabla1Component } from './views/tabla1/tabla1.component';
+import { FinalDatatableComponent } from './views/final-datatable/final-datatable.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Tabla1Component],
+  imports: [RouterOutlet, Tabla1Component, FinalDatatableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { RouterModule, Routes } from '@angular/router';
 import { Tabla1Component } from './views/tabla1/tabla1.component';
 import { NgModule } from '@angular/core';
+import { FinalDatatableComponent } from './views/final-datatable/final-datatable.component';
 
 export const routes: Routes = [
   {
     path: 'tabla1',
     component: Tabla1Component
+  },
+  {
+    path: 'tabla-final',
+    component: FinalDatatableComponent
   }
 ];
 
