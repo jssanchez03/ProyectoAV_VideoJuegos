194af4a1de93055abf650e8311f9b7fbc3e8c0c9
JulioNa <janavarrete8@espe.edu.ec>
2024-06-17 20:46:06 -0500

    Tabla 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Tabla1Component } from './tabla1/tabla1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Tabla1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
