36c0367d8c2e96e98b1d42dd2e53a0fb297f8ade
Jair Sánchez <jssanchez9@espe.edu.ec>
2024-06-17 21:29:05 -0500

    Asignación de rutas}

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Tabla1Component } from './tabla1/tabla1.component';
+import { Tabla1Component } from './views/tabla1/tabla1.component';
+import { VoucherComponent } from './components/voucher/voucher.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Tabla1Component],
+  imports: [RouterOutlet, Tabla1Component, VoucherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
