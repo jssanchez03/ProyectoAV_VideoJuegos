994f3e8826ccecfc3c1c3021ada7891404a4f87e
Jair Sánchez <jssanchez9@espe.edu.ec>
2024-06-17 21:55:50 -0500

    Componente navbar 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { Tabla1Component } from './views/tabla1/tabla1.component';
-import { VoucherComponent } from './components/voucher/voucher.component';
 import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   {
     path: 'tabla1',
     component: Tabla1Component
-  },
-  {
-    path: 'voucher',
-    component: VoucherComponent
-  },
+  }
 ];
 
 @NgModule({
