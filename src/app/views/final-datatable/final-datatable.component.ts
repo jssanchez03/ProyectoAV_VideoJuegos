c914ece2d4f63c7d66583137e5128ab957e4a7ae
Jair Sánchez <jssanchez9@espe.edu.ec>
2024-06-17 21:03:50 -0500

    Estructura

---
+++ src/app/views/final-datatable/final-datatable.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-final-datatable',
+  standalone: true,
+  imports: [],
+  templateUrl: './final-datatable.component.html',
+  styleUrl: './final-datatable.component.css'
+})
+export class FinalDatatableComponent {
+
+}
